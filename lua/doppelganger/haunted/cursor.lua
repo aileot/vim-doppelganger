53c92f08de1e21ae0ba630567ee26c1a6196df03
kaile256 <46470475+kaile256@users.noreply.github.com>
2021-05-30 12:19:20 +0900

    fix(haunted_cursor): insert cache manager in process

--- lua/doppelganger/haunted/cursor.lua
+++ lua/doppelganger/haunted/cursor.lua
@@ -1,15 +1,17 @@
-local vim = vim
+local cmd = vim.cmd
+local g = vim.g
+local fn = vim.fn
+local cache_manager = require('doppelganger.cache.cache_manager').register('haunted_cursor')
 
-local haunted_cursor = {}
 
 --- Convert relative offset into absolute row.
 local apparent_top = function(std_row, offset)
-  local min = vim.fn.line('w0')
+  local min = fn.line('w0')
   local row = std_row
   local cnt = offset
 
   while cnt > 0 and row > min do
-    local foldstart = vim.fn.foldclosed(row)
+    local foldstart = fn.foldclosed(row)
     row = foldstart == -1 and row - 1 or foldstart - 1
     cnt = cnt - 1
   end
@@ -19,29 +21,43 @@ end
 
 --- Convert relative offset into absolute row.
 local apparent_bottom = function(std_row, offset)
-  local max = vim.fn.line('w$')
+  local max = fn.line('w$')
   local row = std_row
   local cnt = offset
 
   while cnt > 0 and row < max do
-    local foldend = vim.fn.foldclosedend(row)
+    local foldend = fn.foldclosedend(row)
     row = foldend == -1 and row + 1 or foldend + 1
     cnt = cnt - 1
   end
 
-  local foldstart = vim.fn.foldclosed(row)
+  local foldstart = fn.foldclosed(row)
   return foldstart == -1 and row or foldstart
 end
 
-function haunted_cursor.update()
-  local current_row = vim.fn.line('.')
-  local offset = vim.g['doppelganger#ego#max_offset']
+
+local M = {}
+
+M.update = function()
+  local current_row = fn.line('.')
+  local offset = g['doppelganger#ego#max_offset']
   local top    = apparent_top(current_row, offset)
   local bottom = apparent_bottom(current_row, offset)
 
-  local min_range = vim.g['doppelganger#ego#min_range_of_pairs']
-  vim.fn['doppelganger#update'](top, bottom, min_range)
+  local min_range = g['doppelganger#ego#min_range_of_pairs']
+  fn['doppelganger#update'](top, bottom, min_range)
   return {top, bottom, min_range}
 end
 
-return haunted_cursor
+
+M.cache_manager = cache_manager
+cmd("augroup doppelganger/cache/haunted_cursor")
+cmd("autocmd!")
+cmd([[autocmd TextChanged,TextChangedI,TextChangedP *
+\ lua require('doppelganger.haunted.cursor').cache_manager:attach(vim.fn.winnr()):drop('range')]])
+cmd([[autocmd FileChangedShellPost *
+\ lua require('doppelganger.haunted.cursor').cache_manager:clear('range')]])
+cmd("augroup END")
+
+
+return M
